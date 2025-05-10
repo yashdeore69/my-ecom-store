f01c9a74ae649846f50e1b82d3c404bcdb12f435
yashdeore69 <deoreyash2003@gmail.com>
2025-05-10 23:08:07 +0530

    first commita: basic setup, login/signup routes and DB schema

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req,res) => {
+    res.send("Signup route called");
+}
+
+export const login = (req,res) => {
+    res.send("Login route called");
+}
+
+export const logout = (req,res) => {
+    res.send("Logout route called");
+}
+
